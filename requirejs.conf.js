9662c3f614ec9070ed03074eed11860bde729efd
Benjamin Goering <bengoering@gmail.com>
2013-09-25 16:11:02 -0700

    Make media wall work with sdk v2.0.0. Bump to v2.0.0-alpha.0

--- requirejs.conf.js
+++ requirejs.conf.js
@@ -9,7 +9,8 @@ require.config({
     'jasmine-html': 'lib/jasmine/lib/jasmine-core/jasmine-html',
     'jasmine-jquery': 'lib/jasmine-jquery/lib/jasmine-jquery',
     'event-emitter': 'lib/event-emitter/src/event-emitter',
-    inherits: 'lib/inherits/inherits'
+    inherits: 'lib/inherits/inherits',
+    json: 'lib/requirejs-plugins/src/json'
   },
   packages: [{
     name: "streamhub-wall",
@@ -17,6 +18,21 @@ require.config({
   },{
     name: "streamhub-sdk",
     location: "lib/streamhub-sdk/src/"
+  },{
+    name: "streamhub-sdk/modal",
+    location: "lib/streamhub-sdk/src/modal"
+  },{
+    name: "streamhub-sdk/collection",
+    location: 'lib/streamhub-sdk/src/collection'
+  },{
+    name: "streamhub-sdk/auth",
+    location: 'lib/streamhub-sdk/src/auth'
+  },{
+    name: "streamhub-sdk/content",
+    location: 'lib/streamhub-sdk/src/content'
+  },{
+    name: 'streamhub-sdk-tests',
+    location: 'lib/streamhub-sdk/tests/'
   },{
     name: "stream",
     location: "lib/stream/src"
--- tests/spec/MediaWallView.js
+++ tests/spec/MediaWallView.js
@@ -2,78 +2,79 @@ define([
     'jasmine',
     'streamhub-wall',
     'streamhub-sdk',
-    '../MockStream',
+    'streamhub-sdk/content',
+    'streamhub-sdk-tests/mocks/mock-attachments-stream',
     'jasmine-jquery'],
-function (jasmine, MediaWallView, Hub, MockStream) {
-describe('A MediaWallView', function () {
-	// construction behavior
-    describe('can be constructed', function() {
-        it ("with no options", function () {
-	        var view = new MediaWallView();
-        	expect(view).toBeDefined();
-    	});
-    	it ("with empty options", function () {
-        	var view = new MediaWallView({});
-        	expect(view).toBeDefined();
-            expect(view._autoFitColumns).toBe(true);
-    	});
-	    it ("with an el", function () {
-	        setFixtures('<div id="hub-MediaWallView"></div>');  
-	        var view = new MediaWallView({
-	            el: $('#hub-MediaWallView')
-	        });
-	        expect(view).toBeDefined();
-	    });
-        it('with .columns option', function () {
-            setFixtures('<div id="hub-MediaWallView"></div>');
-            var view = new MediaWallView({
-                el: $('#hub-MediaWallView'),
-                columns: 8
-            });
-            expect(view._columns).toBe(8);
-            expect(view._autoFitColumns).toBe(false);
-        });
-	});
-	
-	// post construction behavior    
-    describe ("after correct construction", function () {
-	    var view;
-        var streams;
-	    
-	    beforeEach(function() {
-	        setFixtures('<div id="hub-MediaWallView"></div>');
-	        streams = new Hub.StreamManager({main: new MockStream()});
-	        view = new MediaWallView({ el: $('#hub-MediaWallView').get(0) });
-            streams.bind(view);
-		});
-        it ("should contain 50 mock items & childViews after stream start", function () {
-            streams.start();
-        });
-    });
+function (jasmine, MediaWallView, Hub, Content, MockStream) {
+    'use strict';
 
-    describe("when adding content with different .createdAt dates", function () {
-        var view,
-            startDateInt = 1375383041586,
-            date1 = new Date(startDateInt),
-            date2 = new Date(startDateInt + 1 * 10000),
-            date3 = new Date(startDateInt + 2 * 10000),
-            content1, content2, content3;
-        beforeEach(function () {
-            view = new MediaWallView();
-            content1 = new Hub.Content({ body: 'what1' });
-            content1.createdAt = date1;
-            content2 = new Hub.Content({ body: 'what2' });
-            content2.createdAt = date2;
-            content3 = new Hub.Content({ body: 'what3' });
-            content3.createdAt = date3;
-            view.add(content3);
-            view.add(content1);
-            view.add(content2);
+    describe('A MediaWallView', function () {
+        // construction behavior
+        describe('can be constructed', function() {
+            it ("with no options", function () {
+                var view = new MediaWallView();
+                expect(view).toBeDefined();
+            });
+            it ("with empty options", function () {
+                var view = new MediaWallView({});
+                expect(view).toBeDefined();
+            });
+            it ("with an el", function () {
+                setFixtures('<div id="hub-MediaWallView"></div>');
+                var view = new MediaWallView({
+                    el: $('#hub-MediaWallView')
+                });
+                expect(view).toBeDefined();
+            });
         });
-        it("should order .contentViews by .comparator", function () {
-            var sortedContentViews = view.contentViews.slice(0);
-            sortedContentViews.sort(view.comparator);
-            expect(view.contentViews).toEqual(sortedContentViews);
+        
+        // post construction behavior    
+        describe ("after correct construction", function () {
+            var view;
+            var stream;
+            
+            beforeEach(function() {
+                setFixtures('<div id="hub-MediaWallView"></div>');
+                stream = new MockStream();
+                view = new MediaWallView({ el: $('#hub-MediaWallView').get(0) });
+            });
+            it ("should contain 50 mock items & childViews after stream start", function () {
+                var onEnd = jasmine.createSpy('onEnd');
+                stream.pipe(view);
+                stream.on('end', onEnd);
+                waitsFor(function () {
+                    return onEnd.callCount;
+                });
+                runs(function () {
+                    expect(view.contentViews.length).toBe(9);
+                });
+            });
+        });
+
+        describe("when adding content with different .createdAt dates", function () {
+            var view,
+                startDateInt = 1375383041586,
+                date1 = new Date(startDateInt),
+                date2 = new Date(startDateInt + 1 * 10000),
+                date3 = new Date(startDateInt + 2 * 10000),
+                content1, content2, content3;
+            beforeEach(function () {
+                view = new MediaWallView();
+                content1 = new Content({ body: 'what1' });
+                content1.createdAt = date1;
+                content2 = new Content({ body: 'what2' });
+                content2.createdAt = date2;
+                content3 = new Content({ body: 'what3' });
+                content3.createdAt = date3;
+                view.add(content3);
+                view.add(content1);
+                view.add(content2);
+            });
+            it("should order .contentViews by .comparator", function () {
+                var sortedContentViews = view.contentViews.slice(0);
+                sortedContentViews.sort(view.comparator);
+                expect(view.contentViews).toEqual(sortedContentViews);
+            });
         });
     });
 
@@ -120,5 +121,4 @@ describe('A MediaWallView', function () {
             expect(view._columns).toBe(parseInt(12345/400));
         });
     });
-}); 
 });
